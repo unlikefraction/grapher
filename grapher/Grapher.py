4e0e4d95e213d4be42aa5235176385cb63689c92
Shubham Gupta <shubhastro2@gmail.com>
2020-05-26 10:42:27 +0530

    log/ln problem solved

--- grapher/Grapher.py
+++ grapher/Grapher.py
@@ -134,5 +134,5 @@ class Grapher:
 
 
 if __name__ == '__main__':
-    g = Grapher(['log(x)', 'lnx'], lb=-3, ub=3, label=True, lol='lower center', title='Taylor Polynomial for cos(x)')
+    g = Grapher(['cos(x)', '1 - ((x^2)/factorial(2)) + (x^4)/factorial(4)'], lb=-3, ub=3, label=True, lol='lower center', title='Taylor Polynomial for cos(x)')
     g.plot()
